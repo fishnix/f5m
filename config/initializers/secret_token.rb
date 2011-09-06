229f7c0406d3e37c7d2771ebc701822de9b2989a
E Camden Fisher <fish@fishnix.net>
2011-09-06 15:03:29 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+F5m::Application.config.secret_token = 'b6f8159abc9005c1602a2c040f6eb4f48383ce9eee1638390cb6e28002e0d158de9272ef460fee25e299fe7e19ed4d113dc1a7b348c30d728372f1f0dba54712'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+F5m::Application.config.session_store :cookie_store, :key => '_f5m_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# F5m::Application.config.session_store :active_record_store
